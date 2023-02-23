1b8df9f0a73ed84232095bc790720e83cbf003bc
SrLuisito <agueromartinezluis9@gmail.com>
2023-02-22 23:29:41 -0300

    entrega del trabajo

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'view')));
-app.use('/css', express.static(path.join(__dirname, 'style')));
-app.use('/js', express.static(path.join(__dirname, 'javascript')));
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'javascript')));
+app.use(express.static(path.join(__dirname, 'img')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'view/index.html'));
 });
 
 
@@ -15,4 +15,3 @@ app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
 });
 
-    
